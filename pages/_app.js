b28a077c2a416505c0ddb7179f9da40e68c3352d
Bitee-cd <Calebduniya45@gmail.com>
2023-01-03 19:31:26 +0100

    second clean up

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,11 @@
-import { Router } from "next/router";
-import { useState } from "react";
-import Loader from "../components/Loader";
 import "../styles/globals.css";
-import Preloader from "../components/PreLoader";
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
-  Router.events.on("routeChangeStart", (url) => {
-    setLoading(true);
-  });
-  Router.events.on("routeChangeComplete", (url) => {
-    setTimeout(() => {
-      setLoading(false);
-    });
-  });
-  return <>{loading ? <Preloader /> : <Component {...pageProps} />}</>;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
